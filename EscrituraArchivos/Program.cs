abb5b8354aedcc4dd252a26f99abe25f44a84c1b
SystemsMiguel <105815147+SystemsMiguel@users.noreply.github.com>
2022-06-01 11:12:57 -0700

    versionclass

---
+++ EscrituraArchivos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EscrituraArchivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("ejemplo.txt",true);
+            string[] Lines = {
+                "Esto es la primera linea",
+                "Esto es la segunda liena",
+                "Esto es la tercera linea"
+            };
+
+            foreach (string Line in Lines) 
+            {
+                sw.Write(Line);
+            }
+            sw.Close();
+            Console.WriteLine("Escribiendo");
+            Console.ReadLine();
+        }
+    }
+}
